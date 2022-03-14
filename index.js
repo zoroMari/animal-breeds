9bf20b6a23cb4770be68e40b56342e232aca619f
Mariia Moroz <98117895+zoroMari@users.noreply.github.com>
2022-03-14 14:23:35 +0200

    new cards (#6)
    * add info about cat breeds in js
    
    * create catsCards through js

--- index.js
+++ index.js
@@ -1,3 +1,155 @@
+const catBreedsInfo = [
+  {
+    name: 'Bengal',
+    id: 'Bengal',
+    mainImg: 'img/cats/bengal/bengal_main.jpeg',
+    size: 'large',
+    coat: 'medium',
+    energy: 'high',
+    characteristics: `
+      Bengal Cats have an athletic appearance with well-defined muscles, 
+      creating a lean, balanced appearance. They feature high cheekbones, dark markings around the eyes 
+      and the ears are small and pointed but round at the tips. Their coats have a unique, plush feel 
+      unlike any other cat breed.`,
+    lifespan: '12 to 16 years',
+    colors: `
+      One of the most distinctive features of this feline is its unique coat pattern and color.
+      Most Bengal Cats’ coats have rich, highly contrasted shades of bright orange to light brown, 
+      with dark spots or a distinctive marbling pattern. Some even have a “glitter” to the tips of 
+      their fur, which glistens when the light hits it.`,
+  },
+  {
+    name: 'Main Coon',
+    id: 'MainCoon',
+    mainImg: 'img/cats/mainCoon/mainCoon_main.jpeg',
+    size: 'large',
+    coat: 'long',
+    energy: 'medium',
+    characteristics: `
+      The most recognizable characteristics of adult Maine Coon cats are their large size and long, 
+      bushy tail. Their paws are big and well-tufted to be suitable for walking in snow, as can be 
+      seen in both adult and kitten Maine Coon cats. In addition, their ears are tufted, 
+      often with points of hair at the tips.`,
+    lifespan: '10 to 13 years',
+    colors: `
+      The most common color for Maine Coon cats is brown tabby. However, they can be nearly any color 
+      and pattern. Their eye colors, too, have a wide range from blue to green to gold, and can 
+      include odd-eyes (i.e. two different colored eyes).`,
+  },
+  {
+    name: 'Siamese',
+    id: 'Siamese',
+    mainImg: 'img/cats/siameseCat/siameseCat_main.jpeg',
+    size: 'large',
+    coat: 'medium',
+    energy: 'high',
+    characteristics: `
+      This highly intelligent feline will seek out things to occupy her mind. So if you don’t want 
+      your cat getting into mischief, give her puzzle feeders, games and more to keep her entertained.
+      Their point coloring isn’t the only distinguishing feature of Siamese Cats. 
+      They also have a distinct, wedge-shaped head with large, triangular ears. 
+      The Siamese have a long and slim physique, with longer hind legs and a long, thin tail. 
+      Perhaps their most stunning feature is their deep blue eyes, which are almond-shaped and 
+      slightly slanted.`,
+    lifespan: '11 to 15 years',
+    colors: `
+      Their distinct coloration makes Siamese Cats easily recognizable. Their point coloring, 
+      meaning their ears, feet and tails are darker than their body, can range from brown to chocolate 
+      to a blue gray and even lilac. Other point color variations include tabby and other patterns. 
+      The body is light in color and usually appears as ivory, cream and even glacial white.`,
+  },
+  {
+    name: 'Abyssinian',
+    id: 'Abyssinian',
+    mainImg: 'img/cats/abyssinian/abyssinian_main.jpeg',
+    size: 'small',
+    coat: 'medium',
+    energy: 'high',
+    characteristics: `
+      Seemingly always in motion, she’ll slow down occasionally to curl up next to you on the couch 
+      or in bed. Although independent, she does best with another Aby companion to match her 
+      high activity levels while you’re away. Abyssinians love attention from you and respectful 
+      children and get along well with cat-friendly dogs, as well as other pets, like large 
+      parrots and ferrets.
+      The Abyssinian has a wedge-shaped, yet rounded head with broad ears and almond-shaped 
+      eyes in shades of gold or green. Her body is muscular and athletic, but slim, and supported 
+      by fine-boned legs.`,
+    lifespan: '9 to 15 years',
+    colors: `
+      Abyssinians have what is known as a “ticked” coat, which alternates light and dark bands of 
+      color on each hair shaft. The warmth glow of their coat resembles wild cats like cougars. 
+      The coat comes in four primary colors: ruddy brown, red, blue and fawn. 
+      Some associations permit additional colors, however.`,
+  },
+  {
+    name: 'British Shorthair',
+    id: 'BritishShorthair',
+    mainImg: 'img/cats/britishShorthair/britishShorthair_main.jpeg',
+    size: 'large',
+    coat: 'short',
+    energy: 'low',
+    characteristics: `
+      British Shorthairs are round all over, with a broad chest and strong legs. 
+      Their coats are short, thick and plush. Eye color depends on coat color, but you can expect 
+      British Blues to have gold eyes and those with point coloring to have blue eyes. 
+      Other eye colors range from golden to copper.`,
+    lifespan: '12 to 17 years',
+    colors: `
+      Although their coats come in almost every possible color and pattern, the most popular color 
+      is blue (gray). These cats are sometimes referred to as British Blues.`,
+  },
+  {
+    name: 'Sphynx',
+    id: 'Sphynx',
+    mainImg: 'img/cats/sphynx/sphynx_main.jpg',
+    size: 'small',
+    coat: 'short',
+    energy: 'high',
+    characteristics: `
+      The most striking characteristic of the Sphynx is of course her hairlessness, 
+      which exposes her warm, chamois like skin for the world to see, touch and enjoy. 
+      Her wrinkled skin, exaggerated ears and large eyes give her face an otherworldly expression and wisdom.`,
+    lifespan: '8 to 15 years',
+    colors: `
+      Sphynx colors include white, black, blue, red, cream, chocolate, lavender, cinnamon and fawn, 
+      plus various patterns and shadings.`,
+  },
+  {
+    name: 'American Shorthair',
+    id: 'AmericanShorthair',
+    mainImg: 'img/cats/americanShorthair/americanShorthair_main.png',
+    size: 'large',
+    coat: 'short',
+    energy: 'medium',
+    characteristics: `
+      As a working cat, American Shorthairs have a stocky, muscular build. Their muscular legs 
+      lend themselves to the American’s agility and endurance. They have a large head and full face, 
+      medium-sized ears and large, wide eyes.`,
+    lifespan: '15 to 20 years',
+    colors: `
+      White, black, blue, red, cream and silver, plus various patterns and shadings`,
+  },
+  {
+    name: 'Cornish Rex',
+    id: 'CornishRex',
+    mainImg: 'img/cats/cornishRex/cornishRex_main.jpeg',
+    size: 'small',
+    coat: 'short',
+    energy: 'high',
+    characteristics: `
+      Bat-eared, big-eyed and wavy-coated, the Cornish Rex has a distinctive look and exceptionally 
+      silky coat due to not having guard hairs like other breeds. This active cat has a small, 
+      whippetlike body and loves to climb, leap and sprint. With kittenlike antics that last a lifetime, 
+      this feline likes to be where the action is. The Cornish Rex is perfect for those who want a cat 
+      to participate in their family life.`,
+    lifespan: '8 to 15 years',
+    colors: `
+      White, black, blue, red, cream, chocolate, lavender, silver, smoke and tabby, plus various 
+      patterns and shadings`,
+  },
+];
+
+
 function openAnimals(evt, animalName) {
 
   const tabcontent = document.querySelectorAll('.Tabcontent');
@@ -17,6 +169,43 @@ function openAnimals(evt, animalName) {
 document.getElementById('defaultOpen').click();
 
 
+function showCatCards() {
+  const catsBreeds = document.querySelector('.Tabcontent-Cats .Cats-Breeds');
+
+  function createCatCardTemplate(catBreedsInfo) {
+    return `
+      <div class="Breed ${catBreedsInfo.id}" id="${catBreedsInfo.id}" data-size="${catBreedsInfo.size}" 
+        data-coat="${catBreedsInfo.coat}" data-energy="${catBreedsInfo.energy}">
+        <div class="Breed-Img"></div>
+        <div class="Breed-Info">
+          <p class="Breed-Name">${catBreedsInfo.name}</p>
+          <button>
+            <p>Learn more</p>
+            <img src="img/icons/learnMore.svg">
+          </button>
+        </div>
+      </div>
+    `;
+  }
+
+  function insertCatCards() {
+    const template = catBreedsInfo.reduce((acc, curr) => {
+      return `${acc}${createCatCardTemplate(curr)}`;
+    }, '');
+
+    catsBreeds.innerHTML = `
+      ${catsBreeds.innerHTML}
+      ${template}
+    `;
+  }
+
+  insertCatCards();
+}
+
+showCatCards();
+
+
+
 
 function filterCat() {
   const catBreeds = document.querySelectorAll('.Tabcontent-Cats .Breed');
@@ -30,6 +219,8 @@ function filterCat() {
   const globalEnergySelectValue = document.querySelector('.FilterOption-Energy .Select-Value');
   
   const buttonClean = document.querySelector('.Cats-Filter .Clear');
+
+  const background = document.querySelector('.Background');
   
   const state = {
     size: 'all',
@@ -122,4 +313,5 @@ function filterCat() {
 }
 
 filterCat();
+
 
